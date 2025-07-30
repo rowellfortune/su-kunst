9609052d114c90c231a633a59ed9f7575e79af84
rowellfortune <rowell-fortune@hotmail.com>
2025-07-30 16:19:33 -0300

    Revemo some data

--- packages/frontend/src/containers/Landing.tsx
+++ packages/frontend/src/containers/Landing.tsx
@@ -17,35 +17,14 @@ function Landing() {
           <span className="text-lg">Art work</span>
         </div>
         <div className="flex flex-col my-[10px] mx-[16px] text-center">
-<<<<<<< HEAD
-          <p className="text-4xl">283<span className="text-2xl"></span></p>
-          <span className="text-lg">Opencalls</span>
-        </div>
-        <div className="flex flex-col my-[10px] mx-auto text-center">
-          <p className="text-4xl">14+ <span className="text-2xl"></span></p>
-=======
           <p className="text-4xl">283</p>
           <span className="text-lg">Opencalls</span>
         </div>
         <div className="flex flex-col my-[10px] mx-auto text-center">
           <p className="text-4xl">14</p>
->>>>>>> demo
           <span className="text-lg">Organization</span>
         </div>
       </Card>
-
-      {/* Right‐hand panel */}
-      <div className="absolute top-42 hidden right-8 w-72 p-4 bg-white bg-opacity-80 rounded-lg shadow">
-        <h3 className="font-semibold mb-2">Su-Kunst</h3>
-        <ul className="text-gray-800 space-y-1">
-          <li>Talk</li>
-          <li>Rewards</li>
-          <li>Brave VPN</li>
-        </ul>
-        <button className="mt-4 w-full py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700">
-          Start free trial
-        </button>
-      </div>
     </div>
   )
 }
