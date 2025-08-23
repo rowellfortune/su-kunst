dea4ebec70de6c7e90fcf410d78de370ac129126
rowellfortune <rowell-fortune@hotmail.com>
2025-08-23 15:13:22 -0300

    Updated Layout

--- packages/frontend/src/layout/applayout.tsx
+++ packages/frontend/src/layout/applayout.tsx
@@ -8,12 +8,12 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   const {isAuthenticated } = useAppContext();
 
   return (
-    <div className="bg-slate-100 h-screen mx-auto">
+    <div className="bg-slate-100 h-vh mx-auto">
        {isAuthenticated ?  <AppNavbar /> :  <IndexNavbar />}
-        <main className={`flex ${isAuthenticated ?  `px-2`  :  null} flex-col md:flex-row mx-auto`}>
-          {isAuthenticated && <><Sidebar/></>}
+        <main className={`${isAuthenticated ?  `grid grid-cols-24 gap-2`  :  `flex`} `}>
+          {isAuthenticated && <div className="col-span-5"><Sidebar/></div>}
             {children}
-          {isAuthenticated && <><RightSidebar /></> }
+          {isAuthenticated && <div className="col-span-5"><RightSidebar/></div> }
       </main>
     </div>
   )
