c75bac34dc7a97eb941fd583ae778441d2b6caf0
rowellfortune <rowell-fortune@hotmail.com>
2025-08-26 14:25:16 -0300

    Added Delete rest api's

--- packages/functions/src/chat/delete.ts
+++ packages/functions/src/chat/delete.ts
@@ -1,11 +1,12 @@
 import { Resource } from "sst";
-import { Util } from "@su-kunst/core/util";
+import { RestUtil } from "@su-kunst/core/util";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DeleteCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 const dynamoDb = DynamoDBDocumentClient.from(new DynamoDBClient({}));
 
-export const main = Util.handler(async (event) => {
+export const main = RestUtil.restHandler(async (event) => {
+
   const full  = event?.requestContext?.authorizer?.iam?.cognitoIdentity?.amr[2];
   const userId = full.replace(/^.*:/, "");
 
